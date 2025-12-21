4235d3fcb054eafab3f5c2c04f86f94b35aa4717
Ethan Karpeles <ethankarpeles@gmail.com>
2025-12-21 14:44:03 -0600

    Enforce consistent spacing. Add file structure.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/global.css'
+import App from "./App";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <div>Hello world!</div>
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
