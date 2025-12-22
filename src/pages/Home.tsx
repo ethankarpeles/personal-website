a073f59183b5c8e581d7a0d0a64721863feeec3e
Ethan Karpeles <ethankarpeles@gmail.com>
2025-12-22 02:42:12 -0600

    Expand about section. Edit socials.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar.tsx";
 import headshot from '../assets/professional-headshot.png'
-import { AiFillFacebook, AiFillGithub, AiFillLinkedin, AiFillInstagram } from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 export default function Home() {
     return (<>
@@ -10,18 +10,37 @@ export default function Home() {
 }
 
 function About() {
-    const PMG = <a href='https://www.pmg.com/' target="_blank">PMG</a>;
-    const UNT = <a href='https://www.unt.edu/' target="_blank">University of North Texas</a>;
+    const PMG = <a href='https://www.pmg.com/' target='_blank'>
+        PMG
+    </a>;
+    const GLP = <a href='https://www.pmg.com/careers/graduate-opportunities' target='_blank'>
+        PMG's Graduate Leadership Program
+    </a>;
+    const UNT = <a href='https://www.unt.edu/' target='_blank'>
+        University of North Texas
+    </a>;
+    const UNTMath = <a href='https://math.unt.edu/' target='_blank'>
+        UNT Department of Mathematics
+    </a>
+    const Outstanding = <a href='https://newsletter.math.unt.edu/2024/#SecDeptAwards-1' target='_blank'>
+        Outstanding Undergraduate Student
+    </a>;
+    const STaRS = <a href='https://math.unt.edu/research/stars.html' target='_blank'>
+        Students Talk about Research Seminar
+    </a>
+
     return (
         <article>
             <header>
                 <h1>Hi, I'm Ethan Karpeles</h1>
                 <h2><i>Incoming AI & Software Engineer, {PMG}</i></h2>
+                <Socials />
             </header>
             <section>
                 <div>
-                    <p>Welcome! I am an incoming AI & Software Engineer I at {PMG} and a recent math graduate from the {UNT}. Currently, I am preparing for {PMG}'s Graduate Leadership Program.</p>
-                    <Socials />
+                    <p>Welcome! I am an incoming AI & Software Engineer I at {PMG}. Following a rewarding internship with the company, I am preparing to return through {GLP}.</p>
+                    <p>My software engineering journey is grounded in a strong quantitative background at the {UNT}, where I recently graduated with a Bachelor of Science in Mathematics. In 2024, the {UNTMath} recognized my academic and community contributions by naming me {Outstanding}. Beyond the classroom, I also had the opportunity to explore arithmetic geometry through my research in <i>Rational Points on Elliptic Curves</i>, which I had the opportunity of presenting at the {STaRS} (STaRS).</p>
+                    <p>I thrive at the intersection of mathematical theory and practical software engineering. My goal is to leverage my mathematical knowledge to solve complex problems in the marketing technology space.</p>
                 </div>
                 <img src={headshot} className='headshot' />
             </section>
@@ -36,12 +55,6 @@ function Socials() {
         </a>
         <a href='https://github.com/ethankarpeles'>
             <AiFillGithub className='social' />
-        </a>
-        <a href='https://www.facebook.com/ethan.karpeles'>
-            <AiFillFacebook className='social' />
-        </a>
-        <a href='https://www.instagram.com/ethan.karpeles/'>
-            <AiFillInstagram className='social' />
         </a>
     </>;
 }
