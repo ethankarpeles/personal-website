9b6bef7d469ae7fbcf669541f2e6045b6b780021
Ethan Karpeles <ethankarpeles@gmail.com>
2025-12-21 23:52:26 -0600

    Change font size and change unit to rem. Add content to home.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,17 @@ export default function Home() {
 }
 
 function About() {
+    const PMG = <a href='https://www.pmg.com/' target="_blank">PMG</a>;
+    const UNT = <a href='https://www.unt.edu/' target="_blank">University of North Texas</a>;
     return (
         <article>
             <header>
                 <h1>Hi, I'm Ethan Karpeles</h1>
-                <h2><i>Incoming AI & Software Engineer, <a href='https://www.pmg.com/' target="_blank">PMG</a></i></h2>
+                <h2><i>Incoming AI & Software Engineer, {PMG}</i></h2>
             </header>
             <section>
                 <img src={headshot} className='headshot' />
-                <p>Welcome! I am an incoming AI & Software Engineer I at <a href='https://www.pmg.com/' target="_blank">PMG</a> and a recent mathematics graduate from the <a href='https://www.unt.edu/' target="_blank">University of North Texas</a>.</p>
+                <p>Welcome! I am an incoming AI & Software Engineer I at {PMG} and a recent math graduate from the {UNT}. Currently, I am preparing for {PMG}'s Graduate Leadership Program.</p>
             </section>
         </article>
     );
