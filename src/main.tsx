f28bc76c6fb30367e91af53a0dc984f65e5c323f
Ethan Karpeles <ethankarpeles@gmail.com>
2026-01-09 17:15:06 -0600

    Return to HashRouter. Add 404 redirect.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import '@styles/global.css'
 import "katex/dist/katex.min.css";
 import WebsiteRoutes from "./WebsiteRoutes";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <WebsiteRoutes />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
 )
